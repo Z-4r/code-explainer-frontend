4f21ac42b6dc6cdabc6b583d3b0dcab76f947ae2
Z-4r <raunakper777@gmail.com>
2025-07-17 02:45:55 +0530

    editor theme fixed

--- src/components/codeEditor.jsx
+++ src/components/codeEditor.jsx
@@ -2,30 +2,33 @@ import React from 'react'
 import Editor from '@monaco-editor/react'
 import { useDispatch, useSelector } from 'react-redux';
 import { setCode } from '../redux/codeSlice';
+import ThemeToggle from './ThemeToggle';
 
 const CodeEditor = () => {
   const dispatch = useDispatch();
-  const handleChange = (e) => {
-    dispatch(setCode(e.target.value));
+  const handleEditorChange = (value) => {
+    dispatch(setCode(value || ""));
   };
   const code = useSelector((state) => state.code.code);
   const language = useSelector((state) => state.code.language);
 
   
   return (
+    <div style={{ border: '1px solid #ccc', marginTop: '1rem' }}>
     <Editor
       height="400px"
       defaultLanguage={language}
       value={code}
       placeholder="Enter your code"
       onChange={handleChange}
-      theme="vs-dark"
+      theme={theme}
       options={{
         fontSize: 14,
         minimap: { enabled: false },
         scrollBeyondLastLine: false,
       }}
       />
+    </div>
   );
 };
 
